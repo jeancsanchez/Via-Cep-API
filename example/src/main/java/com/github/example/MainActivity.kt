55b80231cef34ed2e42df6b690b94aa3d8e28ef3
jean <jeancs.patricio@gmail.com>
2019-07-24 09:32:19 -0300

    Removendo arquivos de teste do Android.

--- example/src/main/java/com/github/example/MainActivity.kt
+++ example/src/main/java/com/github/example/MainActivity.kt
@@ -1,25 +1,40 @@
 package com.github.example
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import com.github.jeancsanchez.viacepapi.ViaCepRequest
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * @author Jean Carlos (Github: @jeancsanchez)
+ * @date 23/07/19.
+ * Jesus is alive!
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onResume() {
-        super.onResume()
+        edtCep?.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                ViaCepRequest().buscarCepsPorEndereco(
+                    uf = "CE",
+                    cidade = "Fortaleza",
+                    logradouro = "Domingos",
+                    onSuccess = { ceps ->
+                    },
+                    onError = {
+                    })
+            }
 
-        ViaCepRequest().buscarCepsPorEndereco(
-            uf = "CE",
-            cidade = "Fortaleza",
-            logradouro = "Domingos",
-            onSuccess = { ceps ->
-            },
-            onError = {
-            })
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+        })
     }
 }
